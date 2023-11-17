cadc39a7456e268c844188277ead5c2a11240b05
Mircea <mihail.mircea12@gmail.com>
2023-11-17 13:08:35 +0200

    changed a bit the loop and also added the menu logic for the system status

--- homework-6-environment-monitor/homework-6-environment-monitor.ino
+++ homework-6-environment-monitor/homework-6-environment-monitor.ino
@@ -1,3 +1,7 @@
+//todo
+// test memory erase
+// change threshold values ( make them in cm and lumen or smth )
+
 #include "resetData.h"
 #include "sensorSettings.h"
 #include "usefulDefines.h"
@@ -13,14 +17,17 @@
 int g_mainMenu = SELECTED;
 bool g_isOutsideTheMainMenu = false;
 
-//reset data menu related vars
+// reset data menu related vars
 int g_resetDataMenu = NOT_SELECTED;
 
-//sensor menu related vars
+// sensor menu related vars
 int g_sensorMenu = NOT_SELECTED;
 int g_sensorSamplingRate = 1;
 int g_proximityThresholdValue = 500;
 int g_brightnessThresholdValue = 500;
+
+// system status
+int g_systemStatus = NOT_SELECTED;
 
 
 void setup()
@@ -31,7 +38,7 @@ void setup()
 
 void loop()
 {
-    if(Serial.available() > NO_SERIAL_DATA || g_mainMenu == COMING_BACK_TO_MAIN)
+    if(Serial.available() > NO_SERIAL_DATA || g_mainMenu != SELECTED)
     {   
         if(g_mainMenu == SELECTED)
         {
@@ -54,8 +61,9 @@ void enterMainMenu()
             goToResetData();
             break;
 
-        // case SYSTEM_STATUS:
-        //     break;
+        case SYSTEM_STATUS:
+            goToSystemStatus();
+            break;
 
         // case CONTROL_RGB:
         //     break;
@@ -78,4 +86,92 @@ void printMainMenu()
     Serial.print("\nMAIN MENU: \n");
     Serial.print("1 - Tweak sensor settings\n2 - Reset logged data\n");
     Serial.print("3 - Check system status\n4 - Control the RGB LED\n");
+}
+
+#define GET_SENSOR_READINGS 1
+#define PRINT_SENSOR_SETTINGS 2
+#define GO_BACK 3
+
+
+void printSystemStatus()
+{
+    Serial.print("SYSTEM STATUS: \n");
+    Serial.print("1 - Get current sensor readings (press q to quit this state)\n2 - Print sensor settings\n");
+    Serial.print("3 - Go back\n");
+}
+
+void printSensorVariables()
+{
+    Serial.print("\nThe sampling rate is ");
+    Serial.print(g_sensorSamplingRate);
+    Serial.print(" s\n");
+
+    Serial.print("The proximity sensor threshold is ");
+    Serial.print(g_proximityThresholdValue);
+    Serial.print(" <unit>\n");
+
+    Serial.print("The brightness sensor threshold is ");
+    Serial.print(g_brightnessThresholdValue);
+    Serial.print(" <unit>\n");
+
+    printSystemStatus();
+}
+
+void printSensorReadings()
+{
+    if(Serial.available() != NO_SERIAL_DATA)
+    {
+        int userInput = Serial.read();
+        if(userInput == 'q')
+        {
+            // stops the bypass of system status
+            g_systemStatus = SELECTED;
+            Serial.print("Stopping sensor readings...\n");
+            printSystemStatus();
+            return;
+        }
+    }
+
+    Serial.print("it works!\n");
+    delay(1000);
+}
+
+void goToSystemStatus()
+{
+    if(g_systemStatus == NOT_SELECTED)
+    {
+        printSystemStatus();
+        g_systemStatus = SELECTED;
+    }
+
+    // bypass serail available to constantly print sensor readings
+    if(g_systemStatus == GET_SENSOR_READINGS || Serial.available() > NO_SERIAL_DATA)
+    {
+        // if we want to constantly print sensor readings only from the sensor readings can the system status be changed
+        if(g_systemStatus != GET_SENSOR_READINGS)
+        {
+            g_systemStatus = Serial.parseInt();
+        }
+
+        switch (g_systemStatus)
+        {
+            case GET_SENSOR_READINGS:
+                printSensorReadings();
+                break;
+            
+            case PRINT_SENSOR_SETTINGS:
+                printSensorVariables();
+                break;
+
+            case GO_BACK:
+                g_mainMenu = COMING_BACK_TO_MAIN;
+                g_systemStatus = NOT_SELECTED;
+                break;
+
+            default:
+                g_systemStatus = SELECTED;
+                printSystemStatus();
+                break;
+        }
+    }
 }
