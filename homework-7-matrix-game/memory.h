6e5879c94e09191850a94912738bdda9cfb7b8bb
Mircea <mihail.mircea12@gmail.com>
2023-12-11 17:58:14 +0200

    added a separate header for clearer memory access and division

---
+++ homework-7-matrix-game/memory.h
@@ -0,0 +1,38 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+// in this file all the information related to the arduino memory is stored
+
+// all memory will be stored in relation to the previous memory spot in order to prevent
+// aliasing and using more memory than needed
+
+// the start memory address is not zero in order to prevent early wear on the eeprom
+
+// score related
+#define SCORE_MEMORY_ADDRESS 900
+#define NUMBER_OF_SCORES_KEPT 3 
+#define ADDRESS_AFTER_LAST_SCORE (SCORE_MEMORY_ADDRESS + NUMBER_OF_SCORES_KEPT * sizeof(unsigned long))
+
+// brightness and contrast settings all have a size of max 255 (are bytes)
+// because they are dictated by the resolution of the arduino PWM
+#define SIZE_OF_PWM sizeof(byte)
+
+#define EEPROM_MATRIX_BRIGHTNESS_ADDRESS ADDRESS_AFTER_LAST_SCORE
+#define EEPROM_LCD_BRIGHTNESS_ADDRESS (EEPROM_MATRIX_BRIGHTNESS_ADDRESS + SIZE_OF_PWM)
+#define EEPROM_LCD_CONTRAST_ADDRESS (EEPROM_LCD_BRIGHTNESS_ADDRESS + SIZE_OF_PWM)
+
+
+
+/// visual map:
+/*
+---------------- 1024 (Max size)
+...
+---------------- 
+brightness
+settings
+---------------- 924 
+scores kept
+(ulong)
+---------------- 900 (score mem addr)
+*/
+
+#endif
